e0ac2140334135919af842949b6635a63e75a195
Gil Solis <gilsolisuw@gmail.com>
2021-03-02 21:05:18 -0800

    board.js and space.js

--- js/Player.js
+++ js/Player.js
@@ -3,7 +3,15 @@ class Player {
         this.name = name;
         this.id = id;
         this.color = color;
-        this.active = active
-        this.token = []
+        this.active = active;
+        this.token = this.createTokens(21)
+    }
+    createTokens(num) {
+        let tokens = [];
+        for (let i = 0; i < num; i++) {
+            let token = new Token(i, this)
+            tokens.push(token)
+        }
+        return tokens
     }
 }
